44024f7aeef14474f5ca17a8af5c188dc552e718
John Lee <4685john@gmail.com>
2024-02-07 17:02:59 +1100

    takes user input and returns a temp in celcius

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Make a GET request
-fetch("https://api.openweathermap.org/data/2.5/weather?q="
-    + city
-    + "&appid="
-    + weather_api_key)
-        .then(response)
+
+//document.querySelector("#location");
+function search() {
+    const cityName = document.getElementById("cityInput").value;
+    const apiKey = '000dfe93adc5b676a65726ba484bfee6';
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const temp = data.main.temp
+            document.getElementById("weatherInfo").innerText = temp + " degrees celcius"
+        })
+    
+};
+
